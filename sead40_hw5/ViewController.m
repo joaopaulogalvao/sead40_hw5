e3259d3c58e792ce6acceca4eecbcd44116394de
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-08-31 17:57:56 -0700

    Add coordinates and actions to show locations.

--- sead40_hw5/ViewController.m
+++ sead40_hw5/ViewController.m
@@ -27,12 +27,23 @@
   // Dispose of any resources that can be recreated.
 }
 
+
+#pragma mark - My Actions
 - (IBAction)showLA:(id)sender {
+  
+  [self.mapView setRegion:MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2DMake(34.0500, -118.2500), 100000, 100000) animated:true];
+  
 }
 
 - (IBAction)showSeattle:(id)sender {
+  
+  [self.mapView setRegion:MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2DMake(47.6097, -122.3331), 100000, 100000) animated:true];
+  
 }
 
 - (IBAction)showSydney:(id)sender {
+  
+  [self.mapView setRegion:MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2DMake(-33.8650, 151.2094), 100000, 100000) animated:true];
+  
 }
 @end

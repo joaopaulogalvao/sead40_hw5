a0a5473201ee5de7f784f238433b83c692c5dcb1
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-09-01 11:29:01 -0700

    Add location manager cycle on viewDidLoad, in order to show user location. Add locationManager delegate methods.

--- sead40_hw5/ViewController.m
+++ sead40_hw5/ViewController.m
@@ -20,11 +20,28 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+  
+  //Init Location Manager
+  self.locationManager = [[CLLocationManager alloc]init];
+  
+  //Set the delegate
+  self.locationManager.delegate = self;
+  
+  //After checking the location fires the delegate method didChangeAuthorizationStatus
+  [self.locationManager requestWhenInUseAuthorization];
+  
+  //Update location
+  [self.locationManager startUpdatingLocation];
+  
+  //Show user location on the map
+  self.mapView.showsUserLocation = YES;
+  
 }
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
+  
 }
 
 
@@ -46,4 +63,58 @@
   [self.mapView setRegion:MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2DMake(-33.8650, 151.2094), 100000, 100000) animated:true];
   
 }
+
+#pragma mark - Location Manager Delegate
+
+-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+  
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations{
+  
+}
+
+
+-(void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+  
+  if (status==3) {
+    [self.mapView setShowsUserLocation:YES];
+    NSLog(@"Showing User Location");
+  }
+  
+}
+
+
+
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

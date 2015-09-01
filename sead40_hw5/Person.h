a959d09eead3c29244a8a11c15385f01b99ec2a0
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-08-31 17:17:05 -0700

    Add Parse framework files.

---
+++ sead40_hw5/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  sead40_hw5
+//
+//  Created by Joao Paulo Galvao Alves on 8/31/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ sead40_hw5/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  sead40_hw5
+//
+//  Created by Joao Paulo Galvao Alves on 8/31/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
